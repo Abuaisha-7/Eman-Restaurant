d4983b49512822d16792c0397bdd19f81a562bb6
Abuaisha-7 <heyrubedru8086@gmail.com>
2025-12-10 07:58:15 +0300

    editing Header & About us Section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => (
     <div  className='app__wrapper_info' >
       <SubHeading title="Chase the new flavour"/>
       <h1 className='app__header-h1'>The key to Fine Dining</h1>
-      <p className='p__opensans' style={{margin:'2rem 0'}}>
+      <p className='p__opensans text-gray-400' style={{margin:'2rem 0'}}>
         Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus 
       </p>
       <button type='button' className='custom__button'>Explore Menu</button>
